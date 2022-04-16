56e1c4e6e2806f5525c99f775fdcf20451258e84
Eric Felipe <99001809+efss7@users.noreply.github.com>
2022-04-15 23:22:50 -0300

    styled da home e do detalhes

--- src/components/Detalhes/Detalhes.js
+++ src/components/Detalhes/Detalhes.js
@@ -1,35 +1,57 @@
 import React from "react";
-import styledComponents from "styled-components";
 import axios from "axios";
 import { BASE_URL, HEADER } from "../../constants/requests";
+import { ContainerDetalhes, ContainerMaster, Descricao, Data, Valor, Pagamento, Lindo } from "./styled";
+import { } from "@material-ui/icons";
+import { MyButton } from "../Structure/styled";
 
-class Detalhes extends React.Component{
-    state={
-        nome:"",
-        prazo:"",
-        descricao:"",
-        pagamento:""
+
+class Detalhes extends React.Component {
+    state = {
+        nome: "",
+        prazo: "",
+        descricao: "",
+        pagamento: ""
     };
 
-    buscarDetalhes = () =>{
+    buscarDetalhes = () => {
         axios.get(`${BASE_URL}/jobs/${this.props.servicoId}`, HEADER
-        ).then(res => {console.log(res)
-            this.setState({servico: res.data.result.servico})
-        }).catch(err=>{
+        ).then(res => {
+            console.log(res)
+            this.setState({ servico: res.data.result.servico })
+        }).catch(err => {
             console.log(err)
         });
     };
 
-    render(){
-        return(
+    render() {
+        return (
             <div>
-                <h4>{this.props.service.title}</h4>
-                <p>{this.props.service.description}</p>
-                <p>{this.props.convertDate(this.props.service.dueDate)}</p>
-                <p>{this.props.service.price}</p>
-                <p>{this.props.service.paymentMethods}</p>
-
-                <button onClick={() => this.props.mudarTela("detalhes")}>Voltar</button>
+                <ContainerMaster>
+                    <ContainerDetalhes>
+                        <Lindo>
+                            <h2>{this.props.service.title}</h2>
+                            <Descricao>
+                                <b>Descrição: </b>
+                                <p>{this.props.service.description}</p>
+                            </Descricao>
+                            <Data>
+                                <b>Data do serviço: </b>
+                                <p>{this.props.convertDate(this.props.service.dueDate)}</p>
+                            </Data>
+                            <Valor>
+                                <b>Valor cobrado: </b>
+                                <p>{this.props.service.price}</p>
+                            </Valor>
+                            <Pagamento>
+                                {/* quero fazer uma lista com os métodos de pagamento */}
+                                <b>Métodos de pagamento: </b>
+                                <ul><li>{this.props.service.paymentMethods}</li></ul>
+                            </Pagamento>
+                            <MyButton variant="outlined" color="secondary" onClick={() => this.props.mudarTela("detalhes")}>Voltar</MyButton>
+                        </Lindo>
+                    </ContainerDetalhes>
+                </ContainerMaster>
             </div>
         )
     }
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,4 +1,9 @@
 import React from 'react';
+import { CardHome, CardContratar, CardCadastro } from "./styled";
+import {} from "@material-ui/icons";
+import { MyButton } from '../../components/Structure/styled';
+import imgcadastro from '../../assets/imgs/cadastro.png'
+import imgcontatar from '../../assets/imgs/constratar2.png'
 
 export default class Home extends React.Component {
     
@@ -6,15 +11,23 @@ export default class Home extends React.Component {
     
     render() {
         return(
-            <>
-            <button onClick={() => this.props.mudarTela("cadastro")}>
-                Quer ser um ninja
-            </button>
-            <button onClick={() => this.props.mudarTela("contratar")}>
-                Preciso de um ninja
-            </button>
-         
-            </>
+            <CardHome>
+
+                <CardContratar>
+                <img src={imgcontatar}/>
+                <MyButton variant="outlined" color="secondary" onClick={() => this.props.mudarTela("contratar")}>
+                    Contrate um ninja
+                </MyButton>
+                </CardContratar>
+                
+                <CardCadastro>
+                <img src={imgcadastro}/>
+                <MyButton variant="outlined" color="secondary" onClick={() => this.props.mudarTela("cadastro")}>
+                Seja um ninja
+                </MyButton>
+                </CardCadastro>
+
+            </CardHome>
         )
     }
 }
